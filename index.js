0049df14af5402b6d0b2e867cd1b922a43bfc7ee
Lidia Miron <lidiamirion@gmail.com>
2024-06-27 10:16:16 +0200

    carousel de texto

--- index.js
+++ index.js
@@ -56,3 +56,38 @@ function scrollToLocation(location) {
   abrir.classList.add("displayMenu");
   cerrar.classList.remove("displayMenu");
 }
+
+// Bodas Carousel
+$(document).ready(function () {
+  $(".bodas-carousel").slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-custom-arrow slick-prev"> < </button>',
+    nextArrow:
+      '<button type="button" class="slick-custom-arrow slick-next"> > </button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
